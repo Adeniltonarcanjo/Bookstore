fa4604fece0ac5ee91c02fc752fdab5d81fc241e
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-10-28 16:02:37 -0300

    start project

---
+++ Bookstore/src/main/java/github/adeniltonarcanjo/Bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package github.adeniltonarcanjo.Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
