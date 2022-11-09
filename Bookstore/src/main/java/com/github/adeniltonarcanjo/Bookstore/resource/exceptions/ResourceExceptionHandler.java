157d755a709586d2eca079bd929fee3d52dbc3ea
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-11-09 15:31:26 -0300

    Exception for delete category

--- Bookstore/src/main/java/com/github/adeniltonarcanjo/Bookstore/resource/exceptions/ResourceExceptionHandler.java
+++ Bookstore/src/main/java/com/github/adeniltonarcanjo/Bookstore/resource/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.github.adeniltonarcanjo.Bookstore.resource.exceptions;
 
+import com.github.adeniltonarcanjo.Bookstore.services.exceptions.DataIntegrityViolationException;
 import com.github.adeniltonarcanjo.Bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
       StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
               e.getMessage());
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
+    @ExceptionHandler(com.github.adeniltonarcanjo.Bookstore.services.exceptions.DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 
     }
 
--- Bookstore/src/main/java/com/github/adeniltonarcanjo/Bookstore/services/CategoryService.java
+++ Bookstore/src/main/java/com/github/adeniltonarcanjo/Bookstore/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.github.adeniltonarcanjo.Bookstore.services;
 import com.github.adeniltonarcanjo.Bookstore.domain.Category;
 import com.github.adeniltonarcanjo.Bookstore.dtos.CategoryDTO;
 import com.github.adeniltonarcanjo.Bookstore.repositories.CategoryRepository;
+import com.github.adeniltonarcanjo.Bookstore.services.exceptions.DataIntegrityViolationException;
 import com.github.adeniltonarcanjo.Bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,28 +22,34 @@ public class CategoryService {
     public Category findById(Integer id) {
         Optional<Category> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found" +
-                id + ", Type"+ Category.class.getName()));
+                id + ", Type" + Category.class.getName()));
     }
 
-    public List<Category> findAll(){
+    public List<Category> findAll() {
         return repository.findAll();
     }
 
-    public Category create(Category obj){
+    public Category create(Category obj) {
         obj.setId(null);
         return repository.save(obj);
     }
 
-    public Category update(Integer id, CategoryDTO objDto){
-        Category obj= findById(id);
+    public Category update(Integer id, CategoryDTO objDto) {
+        Category obj = findById(id);
         obj.setName(objDto.getName());
         obj.setDescription(objDto.getDescription());
         return repository.save(obj);
     }
 
-    public void delete(Integer id){
+    public void delete(Integer id) {
         findById(id);
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Category don't be deleted");
+
+        }
+
     }
 
 
